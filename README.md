3538a6daa1c0352696c7502cde3ea0267125b1ab
Oskar Truffer <ot@studer-raimann.ch>
2016-07-27 15:34:42 +0200

    Initial commit with folder structure

---
+++ README.md
@@ -0,0 +1,10 @@
+Priority Question Plugin
+--------------------------
+
+Installation
+
+```
+mkdir -p Customizing/global/plugins/Modules/TestQuestionPool/Questions
+cd Customizing/global/plugins/Modules/TestQuestionPool/Questions
+git clone https://github.com/studer-raimann/PriorityQuestion.git
+```
---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+ 
+// alphanumerical ID of the plugin; never change this
+$id = "priq";
+  
+// code version; must be changed for all code changes
+ $version = "0.0.1";
+   
+// ilias min and max version; must always reflect the versions that should
+// run with the plugin
+$ilias_min_version = "5.1.0";
+$ilias_max_version = "5.10.999";
+    
+// optional, but useful: Add one or more responsible persons and a contact email
+$responsible = "Oskar Truffer";
+$responsible_mail = "ot@studer-raimann.ch";
+//    ?>
