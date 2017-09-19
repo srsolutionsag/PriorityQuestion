c9890083049d9026a55bb57d6c4f4b3011fd833a
gcomte <gabriel.comte@gmail.com>
2017-09-19 11:19:16 +0200

    This version of the plugin does NOT work on ILIAS 5.2

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'prioquestion';
 $version = '1.1.8';
 $ilias_min_version = '5.0.0';
-$ilias_max_version = '5.2.999';
+$ilias_max_version = '5.1.999';
 $responsible = 'studer+raimann ag';
 $responsible_mail = 'info@studer-raimann.ch';
