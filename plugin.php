f3ade27640a768a1b7013129f1cf7adc706569b2
Benjamin Seglias <bs@studer-raimann.ch>
2018-08-23 11:23:30 +0200

    use composer and DIC Library in plugin

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'prioquestion';
-$version = '1.2.2';
-$ilias_min_version = '5.0.0';
-$ilias_max_version = '5.2.999';
+$version = '1.2.3';
+$ilias_min_version = '5.2.0';
+$ilias_max_version = '5.3.999';
 $responsible = 'studer+raimann ag';
 $responsible_mail = 'info@studer-raimann.ch';
